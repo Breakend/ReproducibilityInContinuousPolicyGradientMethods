83118c1944403f6f552b0b679aa92c6ea67e29b2
Peter Henderson <peter.henderson@mail.mcgill.ca>
2017-06-16 11:29:28 -0400

    Fix plot results to plot actual episodes

--- plot_results.py
+++ plot_results.py
@@ -9,35 +9,19 @@ from numpy.random import choice
 
 
 
-def single_plot(average_vals, std_dev, env_name, smoothing_window=5, no_show=False, ignore_std=False):
-    fig = plt.figure(figsize=(15, 10))
-    rewards_smoothed_1 = pd.Series(average_vals).rolling(smoothing_window, min_periods=smoothing_window).mean()#[:200]
-
-    cum_rwd_1, = plt.plot(range(len(rewards_smoothed_1)), rewards_smoothed_1, label="Unified On-Policy and Off-Policy DDPG")
-
-
-    if not ignore_std:
-        plt.fill_between(range(len(rewards_smoothed_1)), rewards_smoothed_1 + std_dev,   rewards_smoothed_1 - std_dev, alpha=0.3, edgecolor='blue', facecolor='blue')
-
-    plt.legend(handles=[cum_rwd_1])
-    plt.xlabel("Epsiode")
-    plt.ylabel("Average Return")
-    plt.title("%s Environment"% env_name)
-
-    if no_show:
-        fig.savefig('%s.png' % env_name, dpi=fig.dpi)
-    else:
-        plt.show()
-
-    return fig
-
-
-def multiple_plot(average_vals_list, std_dev_list, other_labels, env_name, smoothing_window=5, no_show=False, ignore_std=False, limit=None):
+def multiple_plot(average_vals_list, std_dev_list, traj_list, other_labels, env_name, smoothing_window=5, no_show=False, ignore_std=False, limit=None):
     fig = plt.figure(figsize=(15, 10))
     colors = ["k", "red", "blue", "green", "magenta", "cyan"]
     color_index = 0
+    ax = plt.subplot() # Defines ax variable by creating an empty plot
 
-    for average_vals, std_dev, label in zip(average_vals_list, std_dev_list, other_labels):
+    # Set the tick labels font
+    for label in (ax.get_xticklabels() + ax.get_yticklabels()):
+        label.set_fontname('Arial')
+        label.set_fontsize(22)
+
+
+    for average_vals, std_dev, label, trajs in zip(average_vals_list, std_dev_list, other_labels, traj_list):
         rewards_smoothed_1 = pd.Series(average_vals).rolling(smoothing_window, min_periods=smoothing_window).mean()[:limit]
         if limit is None:
             limit = len(rewards_smoothed_1)
@@ -46,14 +30,15 @@ def multiple_plot(average_vals_list, std_dev_list, other_labels, env_name, smoot
 
         fill_color = colors[color_index]#choice(colors, 1)
         color_index += 1
-        cum_rwd_1, = plt.plot(range(len(rewards_smoothed_1)), rewards_smoothed_1, label=label, color=fill_color[0])
+        cum_rwd_1, = plt.plot(trajs, rewards_smoothed_1, label=label, color=fill_color[0])
         if not ignore_std:
-            plt.fill_between(range(len(rewards_smoothed_1)), rewards_smoothed_1 + std_dev,   rewards_smoothed_1 - std_dev, alpha=0.3, edgecolor=fill_color, facecolor=fill_color)
+            plt.fill_between(trajs, rewards_smoothed_1 + std_dev,   rewards_smoothed_1 - std_dev, alpha=0.3, edgecolor=fill_color, facecolor=fill_color)
 
-    plt.legend(loc='lower right')
-    plt.xlabel("Epsiode")
-    plt.ylabel("Average Return")
-    plt.title("%s Environment"% env_name)
+    axis_font = {'fontname':'Arial', 'size':'28'}
+    plt.legend(loc='lower right', prop={'size' : 20})
+    plt.xlabel("Episode", **axis_font)
+    plt.ylabel("Average Return", **axis_font)
+    plt.title("%s Environment"% env_name, **axis_font)
 
     if no_show:
         fig.savefig('%s.png' % env_name, dpi=fig.dpi)
@@ -91,34 +76,26 @@ def multiple_plot(average_vals_list, std_dev_list, other_labels, env_name, smoot
 
 import argparse
 parser = argparse.ArgumentParser()
-# example python plot_results.py data/progress.csv "CartPole" --other_paths not_gated/progress.csv --labels "Unified On-Policy and Off-Policy DDPG - Random Sigma" "Unified On-Policy and Off-Policy DDPG - Learned Gated Sigma" --save
-parser.add_argument("path_to_progress_csv")
+parser.add_argument("paths_to_progress_csvs", nargs="+", help="All the csvs")
 parser.add_argument("env_name")
 parser.add_argument("--save", action="store_true")
 parser.add_argument("--ignore_std", action="store_true")
-parser.add_argument('--other_paths', nargs='+', help='List of other progress csv files', required=False)
 parser.add_argument('--labels', nargs='+', help='List of labels to go along with the paths', required=False)
 parser.add_argument('--smoothing_window', default=5, type=int)
 parser.add_argument('--limit', default=None, type=int)
 
 args = parser.parse_args()
 
-data = pd.read_csv(args.path_to_progress_csv)
+avg_rets = []
+std_dev_rets = []
+trajs = []
 
-avg_ret = np.array(data["AverageReturn"])
-std_dev_ret = np.array(data["StdReturn"])
+for o in args.paths_to_progress_csvs:
+    data = pd.read_csv(o)
+    avg_ret = np.array(data["AverageReturn"])
+    std_dev_ret = np.array(data["StdReturn"])
+    trajs.append(np.cumsum(np.array(data["NumTrajs"])))
+    avg_rets.append(avg_ret)
+    std_dev_rets.append(std_dev_ret)
 
-# "Unified On-Policy and Off-Policy DDPG"
-
-if args.other_paths:
-    avg_rets = [avg_ret]
-    std_dev_rets = [std_dev_ret]
-    for o in args.other_paths:
-        data = pd.read_csv(o)
-        avg_ret = np.array(data["AverageReturn"])
-        std_dev_ret = np.array(data["StdReturn"])
-        avg_rets.append(avg_ret)
-        std_dev_rets.append(std_dev_ret)
-    multiple_plot(avg_rets, std_dev_rets, args.labels, args.env_name, smoothing_window=args.smoothing_window, no_show=args.save, ignore_std=args.ignore_std, limit=args.limit)
-else:
-    single_plot(avg_ret, std_dev_ret, args.env_name, no_show=args.save, smoothing_window=args.smoothing_window, ignore_std=args.ignore_std)
+multiple_plot(avg_rets, std_dev_rets, trajs, args.labels, args.env_name, smoothing_window=args.smoothing_window, no_show=args.save, ignore_std=args.ignore_std, limit=args.limit)
